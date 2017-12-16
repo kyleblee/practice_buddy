67c47944791f5314af313729057d35052d533099
kyleblee <hello.kylelee@gmail.com>
2017-12-15 23:24:32 -0500

    Add Tonality and Artist validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,4 +4,7 @@ class Artist < ApplicationRecord
   has_many :genres, through: :artist_genres
   has_many :backing_tracks
   has_many :users, through: :artist_users
+
+  validates :name, presence: true
+  validates :name, uniqueness: { message: "That artist has already been created." }
 end
--- app/models/tonality.rb
+++ app/models/tonality.rb
@@ -1,4 +1,7 @@
 class Tonality < ApplicationRecord
   has_many :licks, through: :lick_tonalities
   has_many :backing_tracks, through: :backing_track_tonalities
+
+  validates :name, presence: true
+  validates :name, uniqueness: { message: "That tonality has already been created." }
 end
