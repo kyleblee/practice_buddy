18b114e48afb2ab8f2142bcaefe37ba29c514321
kyleblee <hello.kylelee@gmail.com>
2017-12-15 23:30:11 -0500

    Add location validation

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,7 @@ class Genre < ApplicationRecord
   has_many :tunes
   has_many :licks, through: :genre_licks
   has_many :backing_tracks
+
+  validates :name, presence: true
+  validates :name, uniqueness: { message: "That genre has already been created." }
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,6 @@
 class Location < ApplicationRecord
   has_many :users
+
+  validates :name, presence: true
+  validates :name, uniqueness: { message: "That location has already been created." }
 end
