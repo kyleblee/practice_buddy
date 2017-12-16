d1a1001c43886c788d96667d156e8db415450675
kyleblee <hello.kylelee@gmail.com>
2017-12-15 22:15:39 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :location
+  belongs_to :location, optional: true
   has_many :licks
   has_many :tunes
   has_many :artists, through: :artist_users
@@ -7,4 +7,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_email_format_of :email, message: "Please enter a valid email address."
+
+  validates :name, presence: true
 end
