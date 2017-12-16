5b67ed025b6b10bcbbe84b51bc2d7c7aff43f54c
kyleblee <hello.kylelee@gmail.com>
2017-12-15 23:10:49 -0500

    Add Lick validations

--- app/models/lick.rb
+++ app/models/lick.rb
@@ -1,7 +1,10 @@
 class Lick < ApplicationRecord
   belongs_to :user
-  belongs_to :artist
+  belongs_to :artist, optional: true
   has_many :tonalities, through: :lick_tonalities
   has_many :backing_tracks, through: :backing_track_tonalities
   has_many :genres, through: :genre_licks
+
+  validates :name, presence: true
+  validates :performance_rating, :inclusion => { :in => 1..5 }, allow_nil: true
 end
