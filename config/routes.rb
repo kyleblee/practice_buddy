46b9e24320f7f76ba5ad99e777e4b400060c7f94
kyleblee <hello.kylelee@gmail.com>
2017-12-15 23:56:09 -0500

    Add nested routes for licks and tunes within users, add /users/sign_up route, drop some filler info in _navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => "welcome#index"
+  get '/' => 'welcome#index'
+
+  get '/users/sign_up' => 'users#new'
+
+  resources :users, only: [:index, :show, :edit, :update, :create] do
+    resources :licks
+    resources :tunes
+  end
 end
