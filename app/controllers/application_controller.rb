6ab73db4f3f442b67442b42d0e06b26f54684bfb
kyleblee <hello.kylelee@gmail.com>
2017-12-15 23:46:12 -0500

    Add navbar partial, create root route / view, set up logged_in? and current_user helper methods in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :logged_in?, :current_user
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 end
