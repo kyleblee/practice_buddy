7d18d15686aa63a1235dc095391a52594b0f8ba7
kyleblee <hello.kylelee@gmail.com>
2017-12-16 00:15:21 -0500

    Add /users/sign_in route, replace signed out version of header with links based on created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'welcome#index'
+  root 'welcome#index'
 
   get '/users/sign_up' => 'users#new'
+  get '/users/sign_in' => 'session#new'
   get '/home' => 'session#index'
   get 'users/sign_out' => 'session#delete'
 
