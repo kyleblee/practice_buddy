a67d76284cde2b74eeb233d6a8b31c6378ad8e14
kyleblee <hello.kylelee@gmail.com>
2017-12-19 16:13:47 -0500

    Build inputs on the 'licks#new' form and begin figuring out params that appear in the controller

--- app/controllers/licks_controller.rb
+++ app/controllers/licks_controller.rb
@@ -8,4 +8,39 @@ class LicksController < ApplicationController
     # Need to add logic that accomodates if filters / sorts have been selected.
     # They are reaching params correctly now.
   end
+
+  def new
+    @lick = Lick.new
+  end
+
+  def create
+    binding.pry
+  end
+
+  # {
+  #   "utf8"=>"✓",
+  #   "authenticity_token"=>"Rark3BuPDKa5+hOmAaO7eyJGGaxxBueOVpr+s0OmGYaZowjqb3itIRc9imipOXHIsB6FGbaPC5EvjG6K0LPaQw==",
+  #   "lick"=>{
+  #     "name"=>"fake name",
+  #     "bpm"=>"",
+  #     "current_key"=>"",
+  #     "link"=>"",
+  #     "artist_id"=>"",
+  #     "artist"=>{"name"=>""},
+  #     "tonalities"=>[""],
+  #     "performance_rating"=>"",
+  #     "last_practiced(1i)"=>"",
+  #     "last_practiced(2i)"=>"",
+  #     "last_practiced(3i)"=>"",
+  #     "scheduled_practice(1i)"=>"",
+  #     "scheduled_practice(2i)"=>"",
+  #     "scheduled_practice(3i)"=>"",
+  #     "description"=>""
+  #   },
+  #     "new_tonalities"=>["", ""],
+  #     "commit"=>"Create Lick",
+  #     "controller"=>"licks",
+  #     "action"=>"create",
+  #     "user_id"=>"2"
+  #     } permitted: false>
 end
