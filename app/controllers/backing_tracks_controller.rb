e1198d1f7318af49a236b8f74cafd6fd1643e03f
kyleblee <hello.kylelee@gmail.com>
2017-12-21 07:57:42 -0500

    Modify the backing_tracks#show action to accommodate both nested and non-nested requests with the same view

--- app/controllers/backing_tracks_controller.rb
+++ app/controllers/backing_tracks_controller.rb
@@ -1,7 +1,7 @@
 class BackingTracksController < ApplicationController
   before_action :authenticate_user!, only: [:show, :new, :create]
   before_action :authenticate_owner!, only: [:update, :edit, :delete]
-  before_action :set_user, only: [:show]
+  before_action :set_backing_track_user, only: [:show]
   before_action :set_backing_track, only: [:show]
 
   def new
@@ -16,5 +16,13 @@ class BackingTracksController < ApplicationController
 
   def set_backing_track
     @backing_track = BackingTrack.find_by(id: params[:id])
+    if @backing_track.nil?
+      flash[:message] = "Hmm, we can't seem to find that backing track."
+      redirect_to backing_tracks_url
+    end
+  end
+
+  def set_backing_track_user
+    @user = User.find_by(id: params[:user_id]) if params[:user_id]
   end
 end
