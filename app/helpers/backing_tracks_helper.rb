b9a62874a1b73f18f7b3affa2b837d95e1a5cd45
kyleblee <hello.kylelee@gmail.com>
2017-12-21 16:23:52 -0500

    Build out list on backing_tracks#index with the scoping occurring in the controller, include #backing_track_header in BackingTrackHelper to dynamically set the header

--- app/helpers/backing_tracks_helper.rb
+++ app/helpers/backing_tracks_helper.rb
@@ -1,3 +1,12 @@
 module BackingTracksHelper
 
+  def backing_track_header(backing_tracks)
+    if params[:user_id].to_i == current_user.id
+      "My Backing Tracks"
+    elsif owner = User.find_by(id: params[:user_id])
+      "#{owner.name}'s Backing Tracks"
+    else
+      "Backing Tracks"
+    end
+  end
 end
