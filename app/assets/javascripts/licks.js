d3c252305e57afcff590d740db12ecdd0929deba
kyleblee <hello.kylelee@gmail.com>
2018-01-22 23:02:01 -0500

    Move Filter and Sort form into template to be re-rendered after sorting has taken place. Implement and refactor unsorted lists and sorted by tonality list (both for filtered and unfiltered contexts).

--- app/assets/javascripts/licks.js
+++ app/assets/javascripts/licks.js
@@ -13,27 +13,13 @@ $(document).on('turbolinks:load', function() {
 
     Handlebars.registerPartial('tonality-list-items', document.getElementById('tonality-list-items').innerHTML);
 
-    attachEventListeners();
+    displaySortForm();
 
-    renderLicks(displayUnsortedLicks);
+    renderLicks();
   };
 });
 
-
-function attachEventListeners() {
-  $('form#filter-form').on('submit', function(e) {
-    e.preventDefault();
-
-    const filter = $('select#filter')[0].value;
-    const sort = $('select#sort')[0].value;
-
-    if (sort === "Tonality") {
-      renderLicks(displayTonalitySort);
-    }
-  });
-}
-
-function renderLicks(displayCallback) {
+function renderLicks() {
   // pull user ID from the url for JSON request
   const userId = window.location.href.match(/\/users\/\d\/licks/)[0].match(/\d/)[0];
   const filterAndSortParams = {
@@ -42,18 +28,40 @@ function renderLicks(displayCallback) {
   }
 
   //sends ajax request and delegates to appropriate callback function to display licks
-  $.getJSON(`/users/${userId}/licks`, filterAndSortParams, displayCallback);
+  $.getJSON(`/users/${userId}/licks`, filterAndSortParams, displayCallback(filterAndSortParams));
 }
 
-const displayUnsortedLicks = function(data) {
+function displayCallback(filterAndSortParams) {
+  if (filterAndSortParams["sort"] === "") {
+    return function(data) { displayUnsortedLicks(data, filterAndSortParams); }
+  } else if (filterAndSortParams["sort"] === "Tonality") {
+    return function(data) { displayTonalitySort(data, filterAndSortParams); }
+  } else if (filterAndSortParams["sort"] === "Artist") {
+    // NEED TO KEEP BUILDING OUT THIS CONDITIONAL FLOW BASED ON SORT. REMEMBER THAT FILTER IS ALREADY
+    // TAKEN CARE OF UNLESS THE SORT REQUIRES HEADERS
+  }
+}
+
+function displayUnsortedLicks(data, filterAndSortParams) {
   const template = Handlebars.compile(document.getElementById('unsorted-licks-template').innerHTML);
   const licksHTML = template(data);
   $('#licks').html(licksHTML);
+
+  displaySortForm();
 };
 
-const displayTonalitySort = function(data) {
+function displayTonalitySort (data, filterAndSortParams) {
+  if (filterAndSortParams["filter"] !== "") {
+    for (let tonality in data) {
+      if (tonality !== filterAndSortParams["filter"]) {
+        delete data[tonality];
+      }
+    }
+  }
+
   const template = Handlebars.compile(document.getElementById('tonality-sort-licks-template').innerHTML);
   $('#licks').empty();
+  $('#filter-and-sort-form').empty();
 
   for (let tonality in data) {
     let licksHTML = `<h4>${tonality}</h4><ul>`
@@ -61,4 +69,18 @@ const displayTonalitySort = function(data) {
     licksHTML += `</ul>`
     $('#licks').append(licksHTML);
   }
+
+  displaySortForm();
+}
+
+function displaySortForm() {
+  const template = Handlebars.compile(document.getElementById('sort-form-template').innerHTML)
+  const formHTML = template();
+  $('#filter-and-sort-form').html(formHTML);
+
+  $('form#filter-form').on('submit', function(e) {
+    e.preventDefault();
+
+    renderLicks();
+  });
 }
